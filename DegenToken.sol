3907e9fa6d60434112bbe4b7aa653c254e326211
Sourav Kapoor <136272329+Nashkapz@users.noreply.github.com>
2023-08-09 08:34:35 +0530

    DegenToken.sol

--- DegenToken.sol
+++ DegenToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -10,40 +10,40 @@ contract DegenToken is ERC20, Ownable, ERC20Burnable {
 
     constructor() ERC20("Degen", "DGN") {}
 
-        function mint(address to, uint256 amount) public onlyOwner{
-            _mint(to, amount);
+        function MintingToken(address to, uint128 value) public onlyOwner{
+            _mint(to, value);
         }
-        function transferTokens(address _reciever, uint amount) external{
-            require(balanceOf(msg.sender) >= amount, "you are not owner");
-            approve(msg.sender, amount);
-            transferFrom(msg.sender, _reciever, amount);
+        function TransferingTokens(address _address, uint128 value) external{
+            require(balanceOf(msg.sender) >= value, "You don't have enough tokens to transfer.");
+            approve(msg.sender, value);
+            transferFrom(msg.sender, _address, value);
         }
-        function checkBalance() external view returns(uint){
+        function CheckBalance() external view returns(uint128){
            return balanceOf(msg.sender);
         }
-        function burnTokens(uint amount) external{
-            require(balanceOf(msg.sender)>= amount, "You do not have enough Tokens");
-            _burn(msg.sender, amount);
+        function BurningTokens(uint128 value) external{
+            require(balanceOf(msg.sender)>= value, "You do not have enough tokens to burn.");
+            _burn(msg.sender, value);
         }
-        function gameStore() public pure returns(string memory) {
-            return "1. DegenSword value = 200 \n 2. DegenKick value = 100 \n 3. DegenCape value = 75";
+        function GameStore() public pure returns(string memory) {
+            return "1. UltraDegeniteSword value = 500 \n 2. HydroDegeniteSword value = 250 \n 3. DegeniteSword value = 100";
         }
         function reedemTokens(uint choice) external payable{
-            require(choice<=3,"Invalid selection");
-            if(choice ==1){
-                require(balanceOf(msg.sender)>=200, "Insufficient Balance");
-                approve(msg.sender, 200);
-                transferFrom(msg.sender, owner(), 200);
+            require(choice >= 1 && choice <= 3,"Choose valid Item.");
+            if(choice == 1){
+                require(balanceOf(msg.sender) >= 500, "You don't have required number of tokens in wallet.");
+                approve(msg.sender, 500);
+                transferFrom(msg.sender, owner(), 500);
             }
-            else if(choice ==2){
-                require(balanceOf(msg.sender)>=100, "Insufficient Balance");
-                approve(msg.sender, 100);
-                transferFrom(msg.sender, owner(), 100);
+            else if(choice == 2){
+                require(balanceOf(msg.sender) >= 250, "You don't have required number of tokens in wallet.");
+                approve(msg.sender, 250);
+                transferFrom(msg.sender, owner(), 250);
             }
             else{
-                require(balanceOf(msg.sender)>=75, "Insufficient Balance");
-                approve(msg.sender, 75);
-                transferFrom(msg.sender, owner(), 75);
+                require(balanceOf(msg.sender) >= 100, "You don't have required number of tokens in wallet.");
+                approve(msg.sender, 100);
+                transferFrom(msg.sender, owner(), 100);
             }
 
 
